71e72f32d6b09858bfc82e9b6ed7308b7c096c97
iWilli26 <willi.nguyen@hotmail.fr>
2024-12-19 00:02:40 +0100

    added a comma

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
@@ -126,7 +126,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SELECT] = LAYOUT(
      KC_TAB  , KC_Q ,      S(KC_LEFT)  ,   S(KC_UP)      ,   S(KC_RIGHT)    , KC_W   ,                                       KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  C(S(KC_LEFT))   ,  S(KC_DOWN)     ,   C(S(KC_RIGHT)) , KC_G   ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS  , FKEYS                           KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS  , FKEYS  ,                         KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV     , SYM    ,                               KC_SPC ,KC_RALT, KC_RGUI, KC_APP
     ), 
 
