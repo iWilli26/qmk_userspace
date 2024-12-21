5fb76496c2199a4101364c77ecd8f5be03cfbab6
iWilli26 <willi.nguyen@hotmail.fr>
2024-12-21 01:10:59 +0100

    l

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
@@ -5,9 +5,7 @@
 #include <keycodes.h>
 #include <stdbool.h>
 #include <unistd.h>  // For sleep function
-#include "os_detection.h"
 #include "quantum.h"
-#include "usb_device_state.h"
 #include "host.h"
 #include "print.h"
 
@@ -52,19 +50,16 @@ enum combos {
   AB_ESC,
   JK_TAB,
   QW_SFT,
-  SD_LAYER
 };
 
 const uint16_t PROGMEM ab_combo[] = {CTL_T(KC_E), CTL_T(KC_S), COMBO_END};
 const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
-const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
 
 combo_t key_combos[] = {
   [AB_ESC] = COMBO_ACTION(ab_combo),
   [JK_TAB] = COMBO(jk_combo, KC_TAB),
   [QW_SFT] = COMBO(qw_combo, KC_LSFT),
-  [SD_LAYER] = COMBO(sd_combo, MO(_LAYER)),
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
