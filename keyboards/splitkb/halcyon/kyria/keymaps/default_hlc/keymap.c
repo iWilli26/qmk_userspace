c2f00e5214a054acf0637e3390afe2ddba9222ae
William Nguyen <william.nguyen@ideine.fr>
2025-12-23 09:24:24 +0100

    revert

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keymap.c
@@ -129,8 +129,8 @@ bool process_detected_host_os_user(os_variant_t detected_os) {
                 goto_line_key      = LCTL(KC_G);
                 end_line_key       = KC_END;
                 start_line_key     = KC_HOME;
-                go_back            = LALT(KC_MINS);
-                go_forward         = LALT(S(KC_MINS));
+                go_back            = LCTL(LALT(KC_MINUS));
+                go_forward         = LCTL(S(KC_MINUS));
                 tab_modifier       = KC_LALT; // Use Alt for Linux
                 break;
             case OS_MACOS:
@@ -505,7 +505,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_split_3x6_5_hlc(
       _______, _______, PREV_W, KC_UP, NEXT_W , GOTOLINE,                                            GO_FORWARD, MS_WHLU, MS_UP,   MS_WHLD,  GO_PREV_ERROR, KC_VOLU,
       _______, START_LINE, KC_LEFT, KC_DOWN, KC_RIGHT, END_LINE,                                     GO_BACK, MS_LEFT, MS_DOWN, MS_RGHT, GO_NEXT_ERROR, KC_VOLD,
-      _______, _______, S(KC_F4), KC_F12, KC_F4, _______, _______, KC_SCRL, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, SCREENSHOT, KC_MUTE,
+      _______, _______, S(KC_F4), KC_F4, KC_F12, _______, _______, KC_SCRL, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, SCREENSHOT, KC_MUTE,
                                  _______, _______, _______, _______, _______,MS_BTN3, CTL_CLICK, MS_BTN1, MS_BTN2, _______,
 
         KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
