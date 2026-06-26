da0ba7e20b7c8ef27337c69ea95ff50334154b96
William Nguyen <william.nguyen@ideine.fr>
2026-06-26 09:02:43 +0200

    catch up

--- keyboards/splitkb/halcyon/kyria/keymaps/default_hlc/keycodes.h
+++
@@ -1,9 +0,0 @@
-#define LA_NAV LT(NAV, KC_SPC)
-#define LA_SYM LT(SYM, KC_BSPC)
-#define LA_FN LT(FN, KC_ENT)
-
-#define HOME_R LALT_T(KC_R)
-#define HOME_T LSFT_T(KC_T)
-
-#define HOME_N LSFT_T(KC_N)
-#define HOME_I LALT_T(KC_I)
--- users/halcyon_modules/rules.mk
+++ users/halcyon_modules/rules.mk
@@ -1,39 +1,3 @@
-CURRENT_DIR := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
-
-ERR_COLOR = $(strip $(call make_std_color,1))
-
-SRC += halcyon.c
-
-LTO_ENABLE ?= yes
-
-# May need to be changed when adding more pointing devices
-POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
-CIRQUE_PINNACLE_ATTENUATION = EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_2X
-
-# May need to be changed when adding more displays
-QUANTUM_PAINTER_ENABLE = yes
-QUANTUM_PAINTER_DRIVERS += st7789_spi surface
-
-BACKLIGHT_ENABLE = yes
-BACKLIGHT_DRIVER = pwm
-
-ifdef HLC_ENCODER
-  include $(CURRENT_DIR)/hlc_encoder/rules.mk
-endif
-
-ifdef HLC_TFT_DISPLAY
-  include $(CURRENT_DIR)/hlc_tft_display/rules.mk
-endif
-
-ifdef HLC_CIRQUE_TRACKPAD
-  include $(CURRENT_DIR)/hlc_cirque_trackpad/rules.mk
-endif
-
-HLC_OPTIONS := $(HLC_NONE) $(HLC_CIRQUE_TRACKPAD) $(HLC_ENCODER) $(HLC_TFT_DISPLAY)
-
-ifeq ($(filter 1, $(HLC_OPTIONS)), )
-$(error Halcyon_modules used but wrong or no module specified. Please specify one by adding `-e <module_name>=1` to your compile command where <module_name> can be: HLC_NONE, HLC_CIRQUE_TRACKPAD, HLC_ENCODER or HLC_TFT_DISPLAY)
 # Add this to your existing rules.mk if you have one
 ifneq ($(filter $(strip $(KEYBOARD)), splitkb/halcyon/kyria/rev4 \
                                       splitkb/halcyon/ferris/rev1 \
